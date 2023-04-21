bbebdb8df6785abf16fab4b8ed6df4eee42d2cf1
Robosapien737 <91835339+Robosapien737@users.noreply.github.com>
2023-04-21 18:43:08 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -10,8 +10,9 @@ form.addEventListener('submit', async event => {
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        const proxyUrl = __uv$config.prefix + __uv$config.encodeUrl(url);
+        const newTab = window.open('about:blank', '_blank');
+        newTab.location.href = proxyUrl;
     });
 });
 
